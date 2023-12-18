681907ef12f65026f2605cb203fb5b1d0639aa5f
jojogamezHub <62063539+jojogamezHub@users.noreply.github.com>
2023-12-18 12:32:56 -0500

    Add files via upload

---
+++ src/loggingColoredFormatter.py
@@ -0,0 +1,33 @@
+import logging
+
+
+class ColoredFormatter(logging.Formatter):
+    verbose_notifs = False
+    notifier = str()
+
+    grey = "\x1b[38;21m"
+    blue = "\x1b[38;5;39m"
+    yellow = "\x1b[38;5;226m"
+    red = "\x1b[38;5;196m"
+    boldRed = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.grey + self.fmt + self.reset,
+            logging.INFO: self.blue + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.boldRed + self.fmt + self.reset,
+        }
+
+    def format(self, record):
+        logFmt = self.FORMATS.get(record.levelno)
+
+        if self.verbose_notifs:
+            self.notifier.send(f"[{logging.getLevelName(record.levelno)}] {record.msg}")
+
+        formatter = logging.Formatter(logFmt)
+        return formatter.format(record)
